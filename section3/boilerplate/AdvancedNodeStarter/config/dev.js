b37860354bbbc7b7c16c19f17c6b82ab622f9033
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2024-02-18 18:43:08 +0600

    Compleated Section 3 (Basic Blog App Setup)

---
+++ section3/boilerplate/AdvancedNodeStarter/config/dev.js
@@ -0,0 +1,16 @@
+module.exports = {
+  googleClientID:
+    "516777644641-a91haouoabgee6m7q5hfhnvp05tm2l2o.apps.googleusercontent.com",
+  googleClientSecret: "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",
+  mongoURI:
+    "mongodb+srv://sdesayed24:SayedMongoDB@386@freecluster.mwshian.mongodb.net",
+  cookieKey: "123123123",
+};
+
+
+
+// 516777644641-a91haouoabgee6m7q5hfhnvp05tm2l2o.apps.googleusercontent.com
+// Client secret
+// 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
+// Creation date
+// February 18, 2024 at 5:27:11 PM GMT+6
